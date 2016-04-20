96d55e9ee2567f267c426f959997c3c971ef3c10
ha1fha1f <half.ldk.hiz@gmail.com>
2016-04-20 21:07:23 +0900

    Initial Commit

---
+++ IntroQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntroQuiz
+//
+//  Created by 山口智生 on 2016/04/20.
+//  Copyright © 2016年 ha1f. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
